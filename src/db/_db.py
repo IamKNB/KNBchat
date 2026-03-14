aa78d96f49fdc6d621e924097be0284a7d23c682
knb <craneheart3@gmail.com>
2026-03-14 19:34:06 +0800

    db优化与懒加载

--- src/db/_db.py
+++ src/db/_db.py
@@ -1,15 +1,22 @@
-from typing import Annotated
+from functools import lru_cache
+from typing import Annotated, Iterator
 
 from fastapi import Depends
+from sqlalchemy.engine import Engine
 from sqlmodel import Session, create_engine
 
 from core import get_settings
 
 settings = get_settings()
-engine = create_engine(settings.db_url)
 
 
-def get_session():
+@lru_cache()
+def get_engine() -> Engine:
+    return create_engine(settings.db_url)
+
+
+def get_session() -> Iterator[Session]:
+    engine = get_engine()
     with Session(engine) as session:
         if settings.db_url.startswith("sqlite"):
             session.connection().exec_driver_sql("PRAGMA foreign_keys=ON")
